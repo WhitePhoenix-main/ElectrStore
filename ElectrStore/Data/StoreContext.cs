9966be0500f4cca8ee5a01877cad1c479bb56b84
WhitePhoenix-main <enix.main@gmail.com>
2022-11-02 12:55:56 +0800

    Added project

---
+++ ElectrStore/Data/StoreContext.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+
+namespace ElectrStore;
+
+public class StoreContext : IdentityDbContext
+{
+    public StoreContext(DbContextOptions<StoreContext> options)
+        : base(options)
+    {
+        
+    }
+
+    public DbSet<ProductRecord> ProductRecords { get; set; } = null!;
+}
---
+++ ElectrStore/Pages/Products/Data/ProductRecord.cs
@@ -0,0 +1,10 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace ElectrStore;
+[Table(("Product"))]
+public class ProductRecord
+{
+    [Key] public Guid Id { get; set; }
+    [Column(TypeName = "nvarchar(150)")] public string? ProductName { get; set; }
+}
