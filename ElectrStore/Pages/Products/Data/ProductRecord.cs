0c034427a4b4136a7e89dbcf20efe148dcab2abe
WhitePhoenix-main <enix.main@gmail.com>
2022-11-02 22:14:49 +0800

    ***

--- ElectrStore/Pages/Products/Data/ProductRecord.cs
+++ ElectrStore/Pages/Products/Data/ProductRecord.cs
@@ -5,7 +5,7 @@ namespace ElectrStore;
 [Table(("Product"))]
 public class ProductRecord
 {
-    [Key] public Guid Id { get; set; }
+    [Key] [Column(TypeName = "nvarchar(100)")] public string? Id { get; set; }
     [Column(TypeName = "nvarchar(150)")] public string? ProductName { get; set; }
     public int Price { get; set; }
 }
