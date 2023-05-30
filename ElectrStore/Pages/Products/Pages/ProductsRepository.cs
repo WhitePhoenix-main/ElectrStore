38f0e7166aa9acee52211a090114ab1efa119964
enixm <enix.main@gmail.com>
2023-05-30 17:20:13 +0800

    *Reworked ProductIndexPage

--- ElectrStore/Pages/Products/Pages/ProductsRepository.cs
+++ ElectrStore/Pages/Products/Pages/ProductsRepository.cs
@@ -1,4 +1,11 @@
 ﻿
+using System;
+using System.IO;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.Extensions.Configuration;
+
 namespace ElectrStore
 {
     public interface IProductsRepository
