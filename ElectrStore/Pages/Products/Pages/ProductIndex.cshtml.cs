8bbf162e5c5e49c8e21f3b36a63aaa4fcf73215e
enixm <enix.main@gmail.com>
2023-06-05 22:30:02 +0800

    *Added Paging

--- ElectrStore/Pages/Products/Pages/ProductIndex.cshtml.cs
+++ ElectrStore/Pages/Products/Pages/ProductIndex.cshtml.cs
@@ -1,3 +1,4 @@
+using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 using Microsoft.EntityFrameworkCore;
 
@@ -5,38 +6,81 @@ namespace ElectrStore
 {
     public class ProductIndexModel : PageModel
     {
+        [BindProperty(SupportsGet = true)] public string? SearchText { get; set; }
+        [BindProperty(SupportsGet = true)] public string? RecordType { get; set; }
+        [BindProperty(SupportsGet = true)] public int PageSize { get; set; } = 8;
+        [BindProperty(SupportsGet = true)] public int PageNumber { get; set; } = 1;
+        [BindProperty(SupportsGet = true)] public string SortType { get; set; } = "Price";
         public StoreContext Context { get; }
-
+        public int Counter { get; set;}
+        
+        public int QueryCounter { get; set;}
         public ProductIndexModel(StoreContext context)
         {
             Context = context;
         }
         
-        public IList<ProductRecord> Product { get;set; }
+        public IList<ProductRecord> ProductList { get;set; }
+        
+        public IList<ProductRecord> Query { get; set; }
 
-        public async Task OnGetAsync(string? search, string? productType)
+        public async Task OnGetAsync()
         {
-            ViewData["search"] = search;
-            ViewData["productType"] = productType;
             //TODO: Пересмотреть логику
             var query = Context.ProductRecords.AsNoTracking();
-            if (!string.IsNullOrWhiteSpace(search) && string.IsNullOrWhiteSpace(productType))
+            if (!string.IsNullOrWhiteSpace(SearchText) && string.IsNullOrWhiteSpace(RecordType))
                 query = query
-                    .Where(product => product.CategoryId == search 
-                    || product.ProductName.Contains(search));
-            else if (!string.IsNullOrWhiteSpace(productType) && !string.IsNullOrWhiteSpace(search))
+                    .Where(product => product.CategoryId == SearchText 
+                    || product.ProductName.Contains(SearchText));
+            else if (!string.IsNullOrWhiteSpace(RecordType) && !string.IsNullOrWhiteSpace(SearchText))
                 query = query
-                    .Where(product => product.CategoryId == productType
-                                      || product.ProductName.Contains(search));
-            else if (!string.IsNullOrWhiteSpace(productType) )
+                    .Where(product => product.CategoryId == RecordType
+                                      || product.ProductName.Contains(SearchText));
+            else if (!string.IsNullOrWhiteSpace(RecordType) )
                 query = query
-                    .Where(product => product.CategoryId == productType);
-            
-            Product = await query
-                .OrderBy(product => product.CategoryId)
-                .ThenBy(product => product.ProductName)
-                .ToListAsync();
+                    .Where(product => product.CategoryId == RecordType);
+            if (SortType == "Name")
+            {
+                ProductList = await query
+                    .OrderBy(product => product.CategoryId)
+                    .ThenBy(product => product.ProductName)
+                    .ToListAsync();
+                Counter = ProductList.Count;
+                if (PageNumber - 1 != 0)
+                {
+                    ProductList = ProductList
+                        .Skip(PageSize * (PageNumber - 1))
+                        .ToList();
+                }
+                ProductList =ProductList
+                    .Take(PageSize)
+                    .ToList();
+                QueryCounter = ProductList.Count;
+            }
+            else if(SortType == "Price")
+            {
+                ProductList = await query
+                    .OrderBy(product => product.CategoryId)
+                    .ThenBy(product => product.Price)
+                    .ToListAsync();
+                Counter = ProductList.Count;
+                if (PageNumber - 1 != 0)
+                {
+                    ProductList = ProductList
+                        .Skip(PageSize * (PageNumber - 1))
+                        .ToList();
+                }
+                ProductList =ProductList
+                    .Take(PageSize)
+                    .ToList();
+                
+                QueryCounter = ProductList.Count;
+            }
         }
-        
+
+        public PageResult OnPost()
+        {
+            return Page();
+        }
     }
 }
